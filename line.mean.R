51949065e3d833174c8e30f4dc8bf497af2b25b7
Vitor <vitordsbatista@gmail.com>
2014-10-22 16:32:34 -0300

    media das linhas e colunas

--- line.mean.R
+++ line.mean.R
@@ -3,14 +3,20 @@ line.mean <- function(data){
 	#data <- data[,-1]
   #Cria o dataframe com 28 linhas
   #c <- data.frame(1:28)
-  l <- data.frame(1:28)
+  l <-matrix(ncol=28)
   #Verifica cada linha
-	for (i in c(1:(length(row(data))/785))){
+	for (i in c(1:nrow(data))){
     #Transforma cada linha em uma matrz 28x28
 		q <- matrix(as.numeric(data[i,]), 28, 28)
     #Calcula as medias das colunas de cada linha e concatena
     #c <- cbind(c, colMeans(q))
-    l <- cbind(l, rowMeans(q))
+    #l <- cbind(l, rowMeans(q))
+    
+    a <- rowMeans(q)
+		a <- t(a)
+		#c[i,] <- a
+		a <- rowMeans(q)
+		l <- rbind(l, a)
 	}
-	return(matrix(l))
+	return(l)
 }
